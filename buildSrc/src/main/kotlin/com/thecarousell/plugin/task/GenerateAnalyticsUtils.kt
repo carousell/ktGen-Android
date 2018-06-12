7f6939be01b675b2fffc14cab183bb6185154660
Alexander Sukharev <alexander.sukharev@thecarousell.com>
2018-06-12 13:03:44 +0800

    Refactor utils

--- buildSrc/src/main/kotlin/com/thecarousell/plugin/task/GenerateAnalyticsUtils.kt
+++ buildSrc/src/main/kotlin/com/thecarousell/plugin/task/GenerateAnalyticsUtils.kt
@@ -6,49 +6,35 @@ import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.asTypeName
 
 object GenerateAnalyticsUtils {
+
 	fun createParameterSpec(paramName: String, type: String): ParameterSpec {
-		val dataType: ClassName
-		val defaultValue: String
-		when (type) {
-			"STRING" -> {
-				dataType = String::class.asTypeName().asNullable()
-				defaultValue = "null"
-			}
-			"INTEGER" -> {
-				dataType = Int::class.asTypeName()
-				defaultValue = "0"
-			}
-			"BOOLEAN" -> {
-				dataType = Boolean::class.asTypeName()
-				defaultValue = "false"
-			}
-			else -> {
-				dataType = Int::class.asTypeName()
-				defaultValue = "null"
-			}
-		}
-		return ParameterSpec.builder(paramName, dataType)
-				.defaultValue(defaultValue).build()
+		return ParameterSpec.builder(paramName, getDataType(type))
+				.defaultValue(getDefaultValue(type))
+                .build()
 	}
 
 	fun createProperty(paramName: String, type: String): PropertySpec {
-		val dataType: ClassName
-		when (type) {
-			"STRING" -> {
-				dataType = String::class.asTypeName().asNullable()
-			}
-			"INTEGER" -> {
-				dataType = Int::class.asTypeName()
-			}
-			"BOOLEAN" -> {
-				dataType = Boolean::class.asTypeName()
-			}
-			else -> {
-				dataType = Int::class.asTypeName()
-			}
-		}
-		return PropertySpec.builder(paramName, dataType)
+		return PropertySpec.builder(paramName, getDataType(type))
 				.initializer(paramName)
 				.build()
 	}
+
+    private fun getDataType(type: String): ClassName {
+        return when (type) {
+            "STRING" -> String::class.asTypeName().asNullable()
+            "INTEGER" -> Int::class.asTypeName()
+            "BOOLEAN" -> Boolean::class.asTypeName()
+            else -> Int::class.asTypeName()
+        }
+    }
+
+    private fun getDefaultValue(type: String): String {
+        return when (type) {
+            "STRING" -> "null"
+            "INTEGER" -> "0"
+            "BOOLEAN" -> "false"
+            else -> "null"
+        }
+    }
+
 }
