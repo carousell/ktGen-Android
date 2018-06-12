83f4eb76cdea3d472d3cded2c013ababe47756db
Alexander Sukharev <alexander.sukharev@thecarousell.com>
2018-06-12 14:32:09 +0800

    Add KDoc generation

--- buildSrc/src/main/kotlin/com/thecarousell/plugin/task/generator/AnalyticsGenerator.kt
+++ buildSrc/src/main/kotlin/com/thecarousell/plugin/task/generator/AnalyticsGenerator.kt
@@ -12,10 +12,9 @@ import java.io.File
 class AnalyticsGenerator(private val outDir: File, private val packageName: String) {
 
     fun generateAnalyticsClass(eventList: EventList) {
-        val eventsFileBuilder = FileSpec.builder(packageName,
-                "${eventList.group.toCamelCaseCapitalized()}Events")
-        val eventsObjectBuilder = TypeSpec.objectBuilder(
-                "${eventList.group.toCamelCaseCapitalized()}Events")
+        val objectName = "${eventList.group.toCamelCaseCapitalized()}Events"
+        val eventsFileBuilder = FileSpec.builder(packageName, objectName)
+        val eventsObjectBuilder = TypeSpec.objectBuilder(objectName)
         val modelsFileBuilder = FileSpec.builder(packageName, "AnalyticsModels")
 
         eventList.events.forEach {
@@ -23,33 +22,43 @@ class AnalyticsGenerator(private val outDir: File, private val packageName: Stri
             eventsObjectBuilder.addFunction(createFunction(it))
         }
         eventsFileBuilder.addType(eventsObjectBuilder.build())
+
         FileUtils.writeFiles(outDir, packageName, modelsFileBuilder.build(), eventsFileBuilder.build())
     }
 
     private fun createDataModel(event: Event): TypeSpec {
         val dataModelBuilder = TypeSpec
-                .classBuilder("${event.track.name}Properties".toCamelCaseCapitalized())
+                .classBuilder("${event.track.name.toCamelCaseCapitalized()}Properties")
                 .addModifiers(KModifier.DATA)
+                .addKdoc("Properties of \"${event.track.name}\" event \n\n")
         val constructorBuilder = FunSpec.constructorBuilder()
+
         event.track.properties.forEach {
-            constructorBuilder.addParameter(
-                    GenerateAnalyticsUtils.createParameterSpec(it.name.toCamelCase(), it.type))
-            dataModelBuilder.addProperty(
-                    GenerateAnalyticsUtils.createProperty(it.name.toCamelCase(), it.type))
+            val propertyName = it.name.toCamelCase()
+            constructorBuilder
+                    .addParameter(GenerateAnalyticsUtils.createParameterSpec(propertyName, it.type))
+            dataModelBuilder
+                    .addProperty(GenerateAnalyticsUtils.createProperty(propertyName, it.type))
+                    .addKdoc("@property $propertyName ${it.description}\n")
         }
+
         return dataModelBuilder.primaryConstructor(constructorBuilder.build()).build()
     }
 
     private fun createFunction(event: Event): FunSpec {
         val funBuilder = FunSpec.builder(event.track.name.toCamelCase())
         funBuilder
+                .addKdoc(event.track.condition)
+                .addKdoc("\n")
                 .addParameter("properties",
-                        ClassName(packageName, "${event.track.name}Properties".toCamelCaseCapitalized()))
+                        ClassName(packageName, "${event.track.name.toCamelCaseCapitalized()}Properties"))
                 .addStatement("val map = HashMap<String, Any?>()")
+
         event.track.properties.forEach {
             funBuilder.addStatement("map.put(\"${it.name}\", properties.${it.name.toCamelCase()})")
         }
         funBuilder.addStatement("sendAnalyticsEvent(\"${event.track.name}\", \"${event.track.type}\", map)")
+
         return funBuilder.build()
     }
 
